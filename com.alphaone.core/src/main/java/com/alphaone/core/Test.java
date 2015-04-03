9b55502e3d60d372264b388bbdc80f24e53f5886
Ngoc <Ngoc@192.168.0.19>
2015-04-03 12:30:07 +0200

    Add simple gradle build for jar

---
+++ com.alphaone.core/src/main/java/com/alphaone/core/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.alphaone.core;
+
+/**
+ * @author Ngoc
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
