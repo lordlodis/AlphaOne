5c2e60bf355a174e25a3f06c996b49b9cea86097
Ngoc <Ngoc@192.168.0.19>
2015-04-03 13:11:21 +0200

    Add simple log4j configuration

--- com.alphaone.core/src/main/java/com/alphaone/core/Test.java
+++ com.alphaone.core/src/main/java/com/alphaone/core/Test.java
@@ -3,18 +3,22 @@
  */
 package com.alphaone.core;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author Ngoc
  *
  */
 public class Test {
+	
+	private static final Logger LOGGER = Logger.getLogger(Test.class);
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		LOGGER.info("Hello World !!!");
 	}
 
 }
